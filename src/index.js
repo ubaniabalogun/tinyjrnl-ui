bc45ea485b73e813c32565305a0370b5017fbd81
Ubani Balogun <balogunubani@gmail.com>
2017-06-17 16:19:41 -0700

    Consolidated Android & iOS files; Added NavBar

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5
+  }
+})
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'My Jrnls'
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native! Hello
+        </Text>
+      </View>
+    )
+  }
+}
+
+
+const App = StackNavigator({
+  Home: { screen: HomeScreen }
+})
+
+export default App
